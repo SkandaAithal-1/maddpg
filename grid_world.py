52fae146afb5ba30050a04d13bd333a86507cd9b
SkandaAithal-1 <aithalskanda66@gmail.com>
2024-11-12 13:34:35 +0530

    Remove negative rewards

--- grid_world.py
+++ grid_world.py
@@ -152,9 +152,9 @@ class GridWorld:
         '''
         Takes a step in the environment
         Reward structure:
-          100 -> Goal reached 
-          -10 -> Collision with the walls or other agents
-           -1 -> Everywhere else
+            1 -> Goal reached 
+            0 -> Collision with the walls or other agents
+            0 -> Everywhere else
         
         Actions :
             0 -> Move up
@@ -168,8 +168,8 @@ class GridWorld:
         # for a in range(self.n_agents):
         #     self.env[self.currentPositions[a][0], self.currentPositions[a][1]] = 0
         self.stepCount += 1
-        collisionRew = -1
-        goalRew = 10
+        collisionRew = 0
+        goalRew = 1
         timeRew = 0
         rewards = [0]*self.n_agents
         done = [0]*self.n_agents
